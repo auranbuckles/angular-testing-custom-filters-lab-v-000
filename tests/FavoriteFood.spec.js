94129f75847a9d997823d8326a883881043a28c2
auranbuckles <auran59@gmail.com>
2016-11-15 22:26:00 -0500

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,16 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+	it('should filter an array of objects depending on the favoriteFood property in the object', function () {
+		var mockedList = [
+			{ name: 'Jackie', favoriteFood: 'ice cream'},
+			{ name: 'Jaime', favoriteFood: 'chocolate'},
+			{ name: 'Jack', favoriteFood: 'lemons'}
+		];
+
+		var results = $filter('favoriteFood')(mockedList, 'ice cream');
+		expect(results.length).toBe(1);
+		expect(results[0].name).toBe('Jackie');
+	})
 	
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -7,4 +7,9 @@ describe('removeAllVowels Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+	it('should remove all vowels in a string', function () {
+		var results = $filter('removeAllVowels')('beanie');
+		expect(results).toEqual('bn');
+	});
+
 });
